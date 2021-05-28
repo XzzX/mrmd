29f05e7a56be405d5dd1d004202053810be23439
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-05-28 12:29:14 +0200

    update

--- src/Particles.hpp
+++ src/Particles.hpp
@@ -5,11 +5,13 @@
 class Particles{
 public:
     constexpr static int dim = 3;
+    constexpr static int VectorLength = 8;
 
     auto getPos() {return Cabana::slice<POS>(particles_);}
     auto getVel() {return Cabana::slice<VEL>(particles_);}
     auto getForce() {return Cabana::slice<FORCE>(particles_);}
 
+    auto size() const {return particles_.size();}
     auto numSoA() const {return particles_.numSoA();}
     auto arraySize(size_t s) const {return particles_.arraySize(s);}
 private:
@@ -21,7 +23,6 @@ private:
         FORCE = 2
     };
     using DataTypes = Cabana::MemberTypes<double[dim], double[dim], double[dim]>;
-    constexpr static int VectorLength = 8;
     using ParticlesT = Cabana::AoSoA<DataTypes, DeviceType, VectorLength>;
 
     ParticlesT particles_ = ParticlesT("particles", 100);
--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,38 @@
 
 #include <iostream>
 
+class KFunc
+{
+public:
+    std::shared_ptr<Particles> particles_ = std::make_shared<Particles>();
+    KOKKOS_INLINE_FUNCTION
+    void operator() ( const int s, const int a ) const  {
+        for (int d = 0; d < Particles::dim; ++d)
+            particles_->getPos().access( s, a, d ) += particles_->getVel().access( s, a, d );
+    }
+};
+
+void functor()
+{
+    KFunc func;
+
+    Cabana::SimdPolicy<Particles::VectorLength> simd_policy( 0, func.particles_->size() );
+
+    Cabana::simd_parallel_for( simd_policy, func, "vector_op" );
+    Kokkos::fence();
+}
+
 void aosoa()
 {
     Particles particles;
 
-    for ( std::size_t s = 0; s < particles.numSoA(); ++s )
-        for ( int d = 0; d < particles.dim; ++d )
-            for ( std::size_t a = 0; a < particles.arraySize(s); ++a )
-                particles.getPos().access( s, a, d ) += particles.getVel().access( s, a, d );
+//    for ( std::size_t s = 0; s < particles.numSoA(); ++s )
+//        for ( int d = 0; d < particles.dim; ++d )
+    int s = 0;
+    int d = 0;
+    for ( int a = 0; a < 8; ++a )
+        particles.getPos().access( s, a, d ) += particles.getVel().access( s, a, d );
+
 }
 
 int main(int argc, char* argv[])
