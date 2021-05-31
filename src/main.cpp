bd4798e123907b05a2538726312b437fedf53f17
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-05-31 10:10:51 +0200

    udpate

--- src/main.cpp
+++ src/main.cpp
@@ -30,20 +30,54 @@ void aosoa()
 {
     Particles particles;
 
-//    for ( std::size_t s = 0; s < particles.numSoA(); ++s )
-//        for ( int d = 0; d < particles.dim; ++d )
-    int s = 0;
-    int d = 0;
-    for ( int a = 0; a < 8; ++a )
-        particles.getPos().access( s, a, d ) += particles.getVel().access( s, a, d );
+    auto pos = particles.getPos();
+    auto vel = particles.getVel();
+    for ( std::size_t s = 0; s < particles.numSoA(); ++s )
+        for ( int d = 0; d < particles.dim; ++d )
+            for ( int a = 0; a < 32; ++a )
+                pos.access( s, a, d ) += vel.access( s, a, d );
+    std::cout << pos.access( 0, 0, 0 ) << std::endl;
+}
 
+void kokkos()
+{
+    using VecView = Kokkos::View<double**, Kokkos::LayoutRight>;
+
+    VecView pos = VecView("pos", 3, 100);
+    VecView vel = VecView("pos", 3, 100);
+
+    for ( int d = 0; d < 3; ++d )
+        for ( int a = 0; a < 100; ++a )
+            pos(d, a) += vel( d, a );
+    std::cout << &pos( 0, 0 ) << std::endl;
+    std::cout << &pos( 0, 1 ) << std::endl;
+}
+
+void kokkos_for()
+{
+    using VecView = Kokkos::View<double**, Kokkos::LayoutRight>;
+
+    VecView pos = VecView("pos", 3, 100);
+    VecView vel = VecView("pos", 3, 100);
+
+    Kokkos::parallel_for("loop",
+                         100,
+                         KOKKOS_LAMBDA(const int idx)
+                         {
+                           pos(0, idx) += vel( 0, idx );
+                           pos(1, idx) += vel( 1, idx );
+                           pos(2, idx) += vel( 2, idx );
+                         });
+
+    std::cout << &pos( 0, 0 ) << std::endl;
+    std::cout << &pos( 0, 1 ) << std::endl;
 }
 
 int main(int argc, char* argv[])
 {
     Kokkos::ScopeGuard scope_guard(argc, argv);
 
-    aosoa();
+    kokkos_for();
 
     return EXIT_SUCCESS;
 }
