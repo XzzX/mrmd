c98eb34089f5870533d9cdbfabd43965fcd0cbee
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-06-02 14:21:17 +0200

    added cabana

--- examples/Vectorization/vec.cpp
+++ examples/Vectorization/vec.cpp
@@ -1,7 +1,26 @@
 #include <Kokkos_Core.hpp>
 
+#include "Cabana_AoSoA.hpp"
+
 #include "checks.hpp"
 #include "datatypes.hpp"
+
+class CabanaVec
+{
+public:
+    constexpr static idx_t size = 10000;
+    constexpr static idx_t dim = 3;
+    constexpr static int VectorLength = 8;
+
+    using DeviceType = Kokkos::Device<Kokkos::Serial, Kokkos::HostSpace>;
+    using DataTypes = Cabana::MemberTypes<double[dim]>;
+    using ParticlesT = Cabana::AoSoA<DataTypes, DeviceType, VectorLength>;
+
+    real_t& get(const idx_t idx, const idx_t dim) const { return Cabana::slice<0>(vec_)(idx, dim); }
+
+private:
+    ParticlesT vec_ = ParticlesT("vec", size);
+};
 
 class KokkosVec
 {
@@ -10,17 +29,18 @@ public:
     constexpr static idx_t dim = 3;
     using VectorView = Kokkos::View<real_t**>;
 
-    real_t& get(const idx_t idx, const idx_t dim) const { return vec_(idx, dim); }
+    real_t& get(const idx_t idx, const idx_t dim) const { return vec_(dim, idx); }
 
 private:
-    VectorView vec_ = VectorView("vec", size, dim);
+    VectorView vec_ = VectorView("vec", dim, size);
 };
 
-void kokkos()
+template <typename VECTOR_T>
+void test_vector()
 {
-    KokkosVec vec;
+    VECTOR_T vec;
     Kokkos::parallel_for(
-        Kokkos::RangePolicy<Kokkos::Serial>(0, KokkosVec::size),
+        Kokkos::RangePolicy<Kokkos::Serial>(0, VECTOR_T::size),
         KOKKOS_LAMBDA(const idx_t idx)
         {
             vec.get(idx, 0) += 5;
@@ -37,7 +57,7 @@ int main(int argc, char* argv[])
 {
     Kokkos::ScopeGuard scope_guard(argc, argv);
 
-    kokkos();
+    test_vector<CabanaVec>();
 
     return EXIT_SUCCESS;
 }
