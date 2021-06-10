bfed914fa12d7cc4bf3658989d857d74c8e70815
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-06-10 13:55:15 +0200

    update

--- src/AccumulateForce.hpp
+++ src/AccumulateForce.hpp
@@ -12,19 +12,17 @@ public:
     KOKKOS_INLINE_FUNCTION
     void operator()(const idx_t& idx) const
     {
-        if (particles_.getGhost()(idx) != -1)
+        if (particles_.getGhost()(idx) == -1) return;
+
+        auto realIdx = particles_.getGhost()(idx);
+        ASSERT_EQUAL(
+            particles_.getGhost()(realIdx), -1, "We do not want to add forces to ghost particles!");
+        for (auto dim = 0; dim < Particles::dim; ++dim)
         {
-            auto realIdx = particles_.getGhost()(idx);
-            for (auto dim = 0; dim < Particles::dim; ++dim)
-            {
-                particles_.getForce()(realIdx, dim) += particles_.getForce()(idx, dim);
-                particles_.getForce()(idx, dim) = 0_r;
-            }
+            particles_.getForce()(realIdx, dim) += particles_.getForce()(idx, dim);
+            particles_.getForce()(idx, dim) = 0_r;
         }
     }
 
-    AccumulateForce(Particles& particles)
-        : particles_(particles)
-    {
-    }
+    AccumulateForce(Particles& particles) : particles_(particles) {}
 };
