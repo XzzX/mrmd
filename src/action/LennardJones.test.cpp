6852181f5f2618a7b617050dd4d0d1f75c8105c3
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-06-21 15:09:50 +0200

    update

--- src/action/LennardJones.test.cpp
+++ src/action/LennardJones.test.cpp
@@ -10,5 +10,4 @@ TEST(LennardJones, Equilibrium)
     LennardJones LJ(particles, 2.5_r * sigma, sigma, epsilon);
     std::cout << LJ.computeForce(2, 0, 0) << std::endl;
     EXPECT_FLOAT_EQ(LJ.computeForce(std::pow(2, 1_r / 6_r) * sigma, 0_r, 0_r) + 1_r, 1_r);
-    EXPECT_FLOAT_EQ(LJ.computeForce(sigma, 0_r, 0_r), epsilon);
 }
