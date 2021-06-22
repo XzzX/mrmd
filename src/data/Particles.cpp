3ef3b482ef11e3e588e0297fe67f0ca89cbe517f
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-06-22 17:15:48 +0200

    update

--- src/data/Particles.cpp
+++ src/data/Particles.cpp
@@ -4,7 +4,7 @@ void Particles::copy(const idx_t dst, const idx_t src)
 {
     for (auto dim = 0; dim < 3; ++dim)
     {
-        getPos()(dst, dim) = getPos()(src, dim);
-        getVel()(dst, dim) = getVel()(src, dim);
+        pos(dst, dim) = pos(src, dim);
+        vel(dst, dim) = vel(src, dim);
     }
 }
