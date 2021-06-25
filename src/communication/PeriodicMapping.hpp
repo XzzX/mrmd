309f2ba1feb6aa87d5e4058f55fd30810469a669
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-06-25 09:56:31 +0200

    enforced better mapping

--- src/communication/PeriodicMapping.hpp
+++ src/communication/PeriodicMapping.hpp
@@ -10,16 +10,37 @@ private:
     const Subdomain subdomain_;
 
 public:
+    /**
+     *
+     * @pre Particle position is at most one periodic copy away
+     * from the subdomain.
+     * @post Particle position lies within half-open interval
+     * [min, max) for all coordinate dimensions.
+     */
     KOKKOS_INLINE_FUNCTION
     void operator()(const idx_t& idx) const
     {
         for (auto dim = 0; dim < Particles::DIMENSIONS; ++dim)
         {
             auto& x = pos_(idx, dim);
-            if (x > subdomain_.maxCorner[dim]) x -= subdomain_.diameter[dim];
-            if (x < subdomain_.minCorner[dim]) x += subdomain_.diameter[dim];
-            assert(x <= subdomain_.maxCorner[dim]);
-            assert(x >= subdomain_.minCorner[dim]);
+            if (subdomain_.maxCorner[dim] <= x)
+            {
+                x -= subdomain_.diameter[dim];
+                if (x < subdomain_.minCorner[dim])
+                {
+                    x = subdomain_.minCorner[dim];
+                }
+            }
+            if (x < subdomain_.minCorner[dim])
+            {
+                x += subdomain_.diameter[dim];
+                if (subdomain_.maxCorner[dim] <= x)
+                {
+                    x = subdomain_.minCorner[dim];
+                }
+            }
+            assert(x < subdomain_.maxCorner[dim]);
+            assert(subdomain_.minCorner[dim] <= subdomain_.minCorner[dim]);
         }
     }
 
