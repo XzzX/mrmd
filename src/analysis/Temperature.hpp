ceda1fb0ba527f75f2867d526893ede40232b7ed
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-06-28 09:47:02 +0200

    refactored getTemperature

--- src/analysis/Temperature.hpp
+++ src/analysis/Temperature.hpp
@@ -6,13 +6,11 @@ inline real_t getTemperature(Particles& particles)
 {
     auto vel = particles.getVel();
     real_t velSqr = 0_r;
-    Kokkos::parallel_reduce(
-        Kokkos::RangePolicy<>(0, particles.numLocalParticles),
-        KOKKOS_LAMBDA(const idx_t idx, real_t& sum) {
-            sum +=
-                vel(idx, 0) * vel(idx, 0) + vel(idx, 1) * vel(idx, 1) + vel(idx, 2) * vel(idx, 2);
-        },
-        velSqr);
-
+    auto policy = Kokkos::RangePolicy<>(0, particles.numLocalParticles);
+    auto kernel = KOKKOS_LAMBDA(const idx_t idx, real_t& sum)
+    {
+        sum += vel(idx, 0) * vel(idx, 0) + vel(idx, 1) * vel(idx, 1) + vel(idx, 2) * vel(idx, 2);
+    };
+    Kokkos::parallel_reduce("getTemperature", policy, kernel, velSqr);
     return velSqr / (3.0 * particles.numLocalParticles);
 }
