ef34aff841ef59f111b48df723d89e5ce6931715
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-07-20 13:23:13 +0200

    corrected dump

--- src/io/DumpCSV.cpp
+++ src/io/DumpCSV.cpp
@@ -14,7 +14,7 @@ void dumpCSV(const std::string& filename, data::Particles& particles)
     auto vel = particles.getVel();
 
     std::ofstream fout(filename);
-    fout << "pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, is_ghost" << std::endl;
+    fout << "pos_x, pos_y, pos_z, vel_x, vel_y, vel_z" << std::endl;
     for (idx_t idx = 0; idx < particles.numLocalParticles + particles.numGhostParticles; ++idx)
     {
         fout << pos(idx, 0) << ", " << pos(idx, 1) << ", " << pos(idx, 2) << ", " << vel(idx, 0)
