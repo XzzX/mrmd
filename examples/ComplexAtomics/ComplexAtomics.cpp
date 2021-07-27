954676aa38f96e288f7ceb10df7e4f37d8590922
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-07-27 18:22:55 +0200

    using custom atomic class

--- examples/ComplexAtomics/ComplexAtomics.cpp
+++ examples/ComplexAtomics/ComplexAtomics.cpp
@@ -4,20 +4,53 @@
 
 using namespace mrmd;
 
+struct DoubleCounter
+{
+    idx_t first = 0;
+    idx_t second = 0;
+
+    KOKKOS_INLINE_FUNCTION
+    DoubleCounter() = default;
+    KOKKOS_INLINE_FUNCTION
+    DoubleCounter(idx_t f, idx_t s) : first(f), second(s) {}
+    KOKKOS_INLINE_FUNCTION
+    DoubleCounter(const DoubleCounter& rhs) = default;
+
+    KOKKOS_INLINE_FUNCTION
+    DoubleCounter(const volatile DoubleCounter& rhs)
+    {
+        first = rhs.first;
+        second = rhs.second;
+    }
+
+    KOKKOS_INLINE_FUNCTION
+    void operator=(const DoubleCounter& rhs) volatile
+    {
+        first = rhs.first;
+        second = rhs.second;
+    }
+};
+
+DoubleCounter operator+(const DoubleCounter& lhs, const DoubleCounter& rhs)
+{
+    return DoubleCounter(lhs.first + rhs.first, lhs.second + rhs.second);
+}
+
 void complexAtomics()
 {
-    Kokkos::View<Kokkos::complex<idx_t>> hist("a");
+    Kokkos::View<DoubleCounter> hist("a");
 
     auto policy = Kokkos::RangePolicy<>(0, 1000000);
     auto kernel = KOKKOS_LAMBDA(const idx_t idx)
     {
-        Kokkos::complex<idx_t> val(idx, 2 * idx);
-        atomic_add(&hist(), val);
+        DoubleCounter val(idx, 2 * idx);
+        auto tmp = Kokkos::atomic_fetch_add(&hist(), val);
+        //        hist() = hist() + val;
     };
     Kokkos::parallel_for(policy, kernel);
     Kokkos::fence();
 
-    std::cout << hist().real() << " | " << hist().imag() << std::endl;
+    std::cout << hist().first << " | " << hist().second << std::endl;
 }
 
 int main(int argc, char* argv[])
