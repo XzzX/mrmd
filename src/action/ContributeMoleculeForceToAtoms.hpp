0729faacc001063620ad71aec74b5e28ef6cf60a
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-08-04 14:52:35 +0200

    refactoring

--- src/action/ContributeMoleculeForceToAtoms.hpp
+++ src/action/ContributeMoleculeForceToAtoms.hpp
@@ -11,34 +11,7 @@ namespace action
 class ContributeMoleculeForceToAtoms
 {
 public:
-    static void update(const data::Molecules& molecules, const data::Particles& atoms)
-    {
-        auto moleculesForce = molecules.getForce();
-        auto moleculesAtomEndIdx = molecules.getAtomsEndIdx();
-
-        auto atomsForce = atoms.getForce();
-        auto atomsRelativeMass = atoms.getRelativeMass();
-
-        auto policy =
-            Kokkos::RangePolicy<>(0, molecules.numLocalMolecules + molecules.numGhostMolecules);
-        auto kernel = KOKKOS_LAMBDA(const idx_t& moleculeIdx)
-        {
-            auto atomsStart = moleculeIdx != 0 ? moleculesAtomEndIdx(moleculeIdx - 1) : 0;
-            auto atomsEnd = moleculesAtomEndIdx(moleculeIdx);
-
-            for (auto atomIdx = atomsStart; atomIdx < atomsEnd; ++atomIdx)
-            {
-                atomsForce(atomIdx, 0) +=
-                    atomsRelativeMass(atomIdx) * moleculesForce(moleculeIdx, 0);
-                atomsForce(atomIdx, 1) +=
-                    atomsRelativeMass(atomIdx) * moleculesForce(moleculeIdx, 1);
-                atomsForce(atomIdx, 2) +=
-                    atomsRelativeMass(atomIdx) * moleculesForce(moleculeIdx, 2);
-            }
-        };
-        Kokkos::parallel_for(policy, kernel, "ContributeMoleculeForceToAtoms::update");
-        Kokkos::fence();
-    }
+    static void update(const data::Molecules& molecules, const data::Particles& atoms);
 };
 }  // namespace action
 }  // namespace mrmd
--- src/action/VelocityVerlet.hpp
+++ src/action/VelocityVerlet.hpp
@@ -12,53 +12,8 @@ namespace action
 class VelocityVerlet
 {
 public:
-    static real_t preForceIntegrate(data::Particles& particles, const real_t dt)
-    {
-        auto dtf(0.5_r * dt);
-        auto dtv(dt);
-        auto pos = particles.getPos();
-        auto vel = particles.getVel();
-        auto force = particles.getForce();
-
-        auto policy = Kokkos::RangePolicy<>(0, particles.numLocalParticles);
-        auto kernel = KOKKOS_LAMBDA(const idx_t& idx, real_t& maxDistSqr)
-        {
-            vel(idx, 0) += dtf * force(idx, 0);
-            vel(idx, 1) += dtf * force(idx, 1);
-            vel(idx, 2) += dtf * force(idx, 2);
-            auto dx = dtv * vel(idx, 0);
-            auto dy = dtv * vel(idx, 1);
-            auto dz = dtv * vel(idx, 2);
-            pos(idx, 0) += dx;
-            pos(idx, 1) += dy;
-            pos(idx, 2) += dz;
-
-            auto distSqr = dx * dx + dy * dy + dz * dz;
-            if (distSqr > maxDistSqr) maxDistSqr = distSqr;
-        };
-        real_t maxDistSqr = 0_r;
-        Kokkos::parallel_reduce(
-            "VelocityVerlet::preForceIntegrate", policy, kernel, Kokkos::Max<real_t>(maxDistSqr));
-        Kokkos::fence();
-        return std::sqrt(maxDistSqr);
-    }
-
-    static void postForceIntegrate(data::Particles& particles, const real_t dt)
-    {
-        auto dtf = 0.5_r * dt;
-        auto vel = particles.getVel();
-        auto force = particles.getForce();
-
-        auto policy = Kokkos::RangePolicy<>(0, particles.numLocalParticles);
-        auto kernel = KOKKOS_LAMBDA(const idx_t& idx)
-        {
-            vel(idx, 0) += dtf * force(idx, 0);
-            vel(idx, 1) += dtf * force(idx, 1);
-            vel(idx, 2) += dtf * force(idx, 2);
-        };
-        Kokkos::parallel_for("VelocityVerlet::postForceIntegrate", policy, kernel);
-        Kokkos::fence();
-    }
+    static real_t preForceIntegrate(data::Particles& particles, const real_t dt);
+    static void postForceIntegrate(data::Particles& particles, const real_t dt);
 };
 }  // namespace action
 }  // namespace mrmd
