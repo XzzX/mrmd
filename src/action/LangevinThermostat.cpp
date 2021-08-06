8e27ae893b321ce6274505f946ae568545427e93
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-08-06 11:32:34 +0200

    avoid capturing this pointer

--- src/action/LangevinThermostat.cpp
+++ src/action/LangevinThermostat.cpp
@@ -11,6 +11,8 @@ void LangevinThermostat::apply(data::Particles& particles)
     auto RNG = randPool_;
     auto vel = particles.getVel();
     auto force = particles.getForce();
+    auto p1 = pref1;  // avoid capturing this pointer
+    auto p2 = pref2;  // avoid capturing this pointer
 
     auto policy = Kokkos::RangePolicy<>(0, particles.numLocalParticles);
     auto kernel = KOKKOS_LAMBDA(const idx_t& idx)
@@ -21,9 +23,9 @@ void LangevinThermostat::apply(data::Particles& particles)
         // Get a random number state from the pool for the active thread
         auto randGen = RNG.get_state();
 
-        force(idx, 0) += pref1 * vel(idx, 0) * mass + pref2 * (randGen.drand() - 0.5_r) * massSqrt;
-        force(idx, 1) += pref1 * vel(idx, 1) * mass + pref2 * (randGen.drand() - 0.5_r) * massSqrt;
-        force(idx, 2) += pref1 * vel(idx, 2) * mass + pref2 * (randGen.drand() - 0.5_r) * massSqrt;
+        force(idx, 0) += p1 * vel(idx, 0) * mass + p2 * (randGen.drand() - 0.5_r) * massSqrt;
+        force(idx, 1) += p1 * vel(idx, 1) * mass + p2 * (randGen.drand() - 0.5_r) * massSqrt;
+        force(idx, 2) += p1 * vel(idx, 2) * mass + p2 * (randGen.drand() - 0.5_r) * massSqrt;
 
         // Give the state back, which will allow another thread to acquire it
         RNG.free_state(randGen);
