f7ce8e60d9ed1b1bedb3f22b53cd80d2a58b06e8
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-08-06 11:26:51 +0200

    added missing device functions

--- src/weighting_function/CheckRegion.hpp
+++ src/weighting_function/CheckRegion.hpp
@@ -10,9 +10,15 @@ namespace weighting_function
 {
 constexpr real_t REGION_CHECK_EPSILON = 0_r;
 
-inline bool isInATRegion(const real_t& lambda) { return lambda >= (1_r - REGION_CHECK_EPSILON); }
-inline bool isInCGRegion(const real_t& lambda) { return lambda <= REGION_CHECK_EPSILON; }
-inline bool isInHYRegion(const real_t& lambda)
+KOKKOS_INLINE_FUNCTION bool isInATRegion(const real_t& lambda)
+{
+    return lambda >= (1_r - REGION_CHECK_EPSILON);
+}
+KOKKOS_INLINE_FUNCTION bool isInCGRegion(const real_t& lambda)
+{
+    return lambda <= REGION_CHECK_EPSILON;
+}
+KOKKOS_INLINE_FUNCTION bool isInHYRegion(const real_t& lambda)
 {
     return !isInATRegion(lambda) && !isInCGRegion(lambda);
 }
