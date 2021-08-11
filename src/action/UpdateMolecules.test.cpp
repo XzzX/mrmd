84181f63fe50356112c1221171c0e5bf68b0c73b
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-08-11 11:06:42 +0200

    further refactoring of DiamondFixture

--- src/action/UpdateMolecules.test.cpp
+++ src/action/UpdateMolecules.test.cpp
@@ -2,78 +2,13 @@
 
 #include <gtest/gtest.h>
 
-#include "data/Molecules.hpp"
-#include "data/Particles.hpp"
+#include "test/DiamondFixture.hpp"
 
 namespace mrmd
 {
 namespace action
 {
-/**
- * 2 molecules with 2 atoms each
- * +++++
- * ++A++
- * +B+A+
- * ++B++
- * +++++
- */
-class UpdateMoleculesTest : public ::testing::Test
-{
-protected:
-    static auto getMolecules()
-    {
-        data::Molecules molecules(2);
-
-        auto moleculesAtomsEndIdx = molecules.getAtomsEndIdx();
-        moleculesAtomsEndIdx(0) = 2;
-        moleculesAtomsEndIdx(1) = 4;
-
-        molecules.numLocalMolecules = 2;
-
-        return molecules;
-    }
-
-    static auto getAtoms()
-    {
-        data::Particles atoms(4);
-
-        auto pos = atoms.getPos();
-        pos(0, 0) = 1_r;
-        pos(0, 1) = 0_r;
-        pos(0, 2) = 0_r;
-        atoms.getRelativeMass()(0) = 0.5_r;
-
-        pos(1, 0) = 0_r;
-        pos(1, 1) = 1_r;
-        pos(1, 2) = 0_r;
-        atoms.getRelativeMass()(1) = 0.5_r;
-
-        pos(2, 0) = -1_r;
-        pos(2, 1) = 0_r;
-        pos(2, 2) = 0_r;
-        atoms.getRelativeMass()(2) = 0.5_r;
-
-        pos(3, 0) = 0_r;
-        pos(3, 1) = -1_r;
-        pos(3, 2) = 0_r;
-        atoms.getRelativeMass()(3) = 0.5_r;
-
-        atoms.numLocalParticles = 4;
-
-        return atoms;
-    }
-
-    void SetUp() override
-    {
-        molecules = getMolecules();
-        atoms = getAtoms();
-    }
-
-    // void TearDown() override {}
-
-    data::Molecules molecules = data::Molecules(1);
-    data::Particles atoms = data::Particles(1);
-};
+using UpdateMoleculesTest = test::DiamondFixture;
 
 struct Weight
 {
