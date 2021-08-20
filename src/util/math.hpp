8aefd965e881102f9da3fe3f658b5b971c9b1b58
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-08-20 16:48:06 +0200

    dot product

--- src/util/math.hpp
+++ src/util/math.hpp
@@ -6,7 +6,14 @@ namespace mrmd
 {
 namespace util
 {
+KOKKOS_INLINE_FUNCTION
+real_t dot3(real_t const* const a, real_t const* const b)
+{
+    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+}
+
 KOKKOS_INLINE_FUNCTION real_t sqr(const real_t& sqr) { return sqr * sqr; }
+
 KOKKOS_INLINE_FUNCTION real_t powInt(const real_t& x, const idx_t n)
 {
     auto ww = x;
