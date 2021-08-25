ed402590ef7253346a1b582615e6b84a39dec766
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-08-25 10:21:42 +0200

    introduced mass into LangevinThermostat

--- src/action/LangevinThermostat.cpp
+++ src/action/LangevinThermostat.cpp
@@ -11,21 +11,22 @@ void LangevinThermostat::apply(data::Particles& particles)
     auto RNG = randPool_;
     auto vel = particles.getVel();
     auto force = particles.getForce();
+    auto mass = particles.getMass();
     auto p1 = pref1;  // avoid capturing this pointer
     auto p2 = pref2;  // avoid capturing this pointer
 
     auto policy = Kokkos::RangePolicy<>(0, particles.numLocalParticles);
     auto kernel = KOKKOS_LAMBDA(const idx_t& idx)
     {
-        constexpr real_t mass = 1_r;
-        constexpr real_t massSqrt = 1_r;  // std::sqrt(mass);
+        const real_t m = mass(idx);
+        const real_t mSqrt = std::sqrt(m);
 
         // Get a random number state from the pool for the active thread
         auto randGen = RNG.get_state();
 
-        force(idx, 0) += p1 * vel(idx, 0) * mass + p2 * (randGen.drand() - 0.5_r) * massSqrt;
-        force(idx, 1) += p1 * vel(idx, 1) * mass + p2 * (randGen.drand() - 0.5_r) * massSqrt;
-        force(idx, 2) += p1 * vel(idx, 2) * mass + p2 * (randGen.drand() - 0.5_r) * massSqrt;
+        force(idx, 0) += p1 * vel(idx, 0) * m + p2 * (randGen.drand() - 0.5_r) * mSqrt;
+        force(idx, 1) += p1 * vel(idx, 1) * m + p2 * (randGen.drand() - 0.5_r) * mSqrt;
+        force(idx, 2) += p1 * vel(idx, 2) * m + p2 * (randGen.drand() - 0.5_r) * mSqrt;
 
         // Give the state back, which will allow another thread to acquire it
         RNG.free_state(randGen);
--- src/action/LangevinThermostat.test.cpp
+++ src/action/LangevinThermostat.test.cpp
@@ -3,36 +3,39 @@
 #include <gtest/gtest.h>
 
 #include "data/Particles.hpp"
+#include "test/SingleParticle.hpp"
 
 namespace mrmd
 {
 namespace action
 {
-TEST(LangevinThermostat, Simple)
+using LangevinThermostatTest = test::SingleParticle;
+
+TEST_F(LangevinThermostatTest, Simple)
 {
-    data::Particles particles(1);
-    auto vel = particles.getVel();
-    vel(0, 0) = +2_r;
-    vel(0, 1) = +3_r;
-    vel(0, 2) = +4_r;
-    auto force = particles.getForce();
-    force(0, 0) = +2_r;
-    force(0, 1) = +3_r;
-    force(0, 2) = +4_r;
-    particles.numLocalParticles = 1;
-    particles.numGhostParticles = 0;
-
     LangevinThermostat langevinThermostat(0.5_r, 0.5_r, 0.1_r);
-    langevinThermostat.apply(particles);
+    langevinThermostat.apply(atoms);
 
-    EXPECT_GE(force(0, 0), 1_r - langevinThermostat.pref2 * 0.5_r);
-    EXPECT_LE(force(0, 0), 1_r + langevinThermostat.pref2 * 0.5_r);
-
-    EXPECT_GE(force(0, 1), 1.5_r - langevinThermostat.pref2 * 0.5_r);
-    EXPECT_LE(force(0, 1), 1.5_r + langevinThermostat.pref2 * 0.5_r);
-
-    EXPECT_GE(force(0, 2), 2_r - langevinThermostat.pref2 * 0.5_r);
-    EXPECT_LE(force(0, 2), 2_r + langevinThermostat.pref2 * 0.5_r);
+    auto hAoSoA = Cabana::create_mirror_view_and_copy(Kokkos::HostSpace(), atoms.getAoSoA());
+    auto force = Cabana::slice<data::Particles::FORCE>(hAoSoA);
+    EXPECT_GE(force(0, 0),
+              9_r + langevinThermostat.pref1 * 1.5_r * 7_r -
+                  langevinThermostat.pref2 * std::sqrt(1.5_r) * 0.5_r);
+    EXPECT_LE(force(0, 0),
+              9_r + langevinThermostat.pref1 * 1.5_r * 7_r +
+                  langevinThermostat.pref2 * std::sqrt(1.5_r) * 0.5_r);
+    EXPECT_GE(force(0, 1),
+              7_r + langevinThermostat.pref1 * 1.5_r * 5_r -
+                  langevinThermostat.pref2 * std::sqrt(1.5_r) * 0.5_r);
+    EXPECT_LE(force(0, 1),
+              7_r + langevinThermostat.pref1 * 1.5_r * 5_r +
+                  langevinThermostat.pref2 * std::sqrt(1.5_r) * 0.5_r);
+    EXPECT_GE(force(0, 2),
+              8_r + langevinThermostat.pref1 * 1.5_r * 3_r -
+                  langevinThermostat.pref2 * std::sqrt(1.5_r) * 0.5_r);
+    EXPECT_LE(force(0, 2),
+              8_r + langevinThermostat.pref1 * 1.5_r * 3_r +
+                  langevinThermostat.pref2 * std::sqrt(1.5_r) * 0.5_r);
 }
 }  // namespace action
 }  // namespace mrmd
