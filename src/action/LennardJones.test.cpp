240938c323ead4052873638233d7b620a90b6d4a
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-09-14 16:56:27 +0200

    ported LJ test to device

--- src/action/LennardJones.test.cpp
+++ src/action/LennardJones.test.cpp
@@ -14,19 +14,40 @@ TEST(LennardJones, ExplicitComparison)
     constexpr real_t sigma = 3.01_r;
     constexpr real_t rc = 2.5_r * sigma;
     constexpr real_t cappingDistance = 0.1_r;
-    real_t delta = 0.1_r;
-    impl::CappedLennardJonesPotential LJ({cappingDistance}, {rc}, {sigma}, {epsilon}, 1, true);
-
     const auto cutoffPotential =
         4_r * epsilon * (std::pow(sigma / rc, 12) - std::pow(sigma / rc, 6));
-    for (real_t x = cappingDistance + delta; x < rc + 10 * delta; x += delta)
+
+    impl::CappedLennardJonesPotential LJ({cappingDistance}, {rc}, {sigma}, {epsilon}, 1, true);
+
+    constexpr idx_t steps = 100;
+    constexpr real_t delta = 0.1_r;
+    constexpr real_t startingPos = cappingDistance + delta;
+    EXPECT_GT(startingPos + real_c(steps) * delta, rc);
+
+    ScalarView force("force", steps);
+    ScalarView potential("potential", steps);
+
+    auto policy = Kokkos::RangePolicy<>(0, steps);
+    auto kernel = KOKKOS_LAMBDA(const idx_t idx)
     {
+        auto x = startingPos + real_c(idx) * delta;
+        potential(idx) = LJ.computeEnergy(x * x, 0);
+        force(idx) = -x * LJ.computeForce(x * x, 0);
+    };
+    Kokkos::parallel_for(policy, kernel);
+    Kokkos::fence();
+
+    auto hForce = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), force);
+    auto hPotential = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), potential);
+    for (idx_t idx = 0; idx < steps; ++idx)
+    {
+        auto x = startingPos + real_c(idx) * delta;
         auto potential =
             4_r * epsilon * (std::pow(sigma / x, 12) - std::pow(sigma / x, 6)) - cutoffPotential;
-        EXPECT_FLOAT_EQ(potential, LJ.computeEnergy(x * x, 0));
+        EXPECT_FLOAT_EQ(hPotential(idx), potential);
         auto force = 4_r * epsilon * (-12 * std::pow(sigma / x, 12) + 6 * std::pow(sigma / x, 6)) *
                      x / (x * x);
-        EXPECT_FLOAT_EQ(force, -x * LJ.computeForce(x * x, 0));
+        EXPECT_FLOAT_EQ(hForce(idx), force);
     }
 }
 
