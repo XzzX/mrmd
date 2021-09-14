8e86538d9dc148e418ef151d4c6498b6810a9e90
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-09-14 15:40:28 +0200

    removed std::vector from device code

--- src/action/LennardJones.hpp
+++ src/action/LennardJones.hpp
@@ -12,50 +12,57 @@ namespace impl
 class CappedLennardJonesPotential
 {
 private:
-    std::vector<real_t> ff1_;  ///< force factor 1
-    std::vector<real_t> ff2_;  ///< force factor 2
-    std::vector<real_t> ef1_;  ///< energy factor 1
-    std::vector<real_t> ef2_;  ///< energy factor 2
-    std::vector<real_t> rcSqr_;
-    std::vector<real_t> cappingDistance_;
-    std::vector<real_t> cappingDistanceSqr_;
-    std::vector<real_t> cappingCoeff_;
-    std::vector<real_t> shift_;  ///< shifting the potential at rc to zero
-    std::vector<real_t> energyAtCappingPoint_;
+    struct PrecomputedValues
+    {
+        real_t ff1;  ///< force factor 1
+        real_t ff2;  ///< force factor 2
+        real_t ef1;  ///< energy factor 1
+        real_t ef2;  ///< energy factor 2
+        real_t rcSqr;
+        real_t cappingDistance;
+        real_t cappingDistanceSqr;
+        real_t cappingCoeff;
+        real_t shift;  ///< shifting the potential at rc to zero
+        real_t energyAtCappingPoint;
+    };
+    Kokkos::View<PrecomputedValues*> precomputedValues_;
 
 public:
-    const auto& getShift() const { return shift_; }
-
     KOKKOS_INLINE_FUNCTION
     real_t computeForce(const real_t& distSqr, const idx_t& typeIdx) const
     {
-        if (distSqr >= cappingDistanceSqr_[typeIdx])
+        if (distSqr >= precomputedValues_(typeIdx).cappingDistanceSqr)
         {
             // normal LJ force calculation
             auto frac2 = 1_r / distSqr;
             auto frac6 = frac2 * frac2 * frac2;
-            return frac6 * (ff1_[typeIdx] * frac6 - ff2_[typeIdx]) * frac2;
+            return frac6 *
+                   (precomputedValues_(typeIdx).ff1 * frac6 - precomputedValues_(typeIdx).ff2) *
+                   frac2;
         }
 
         // force capping
-        return cappingCoeff_[typeIdx] / std::sqrt(distSqr);
+        return precomputedValues_(typeIdx).cappingCoeff / std::sqrt(distSqr);
     }
 
     KOKKOS_INLINE_FUNCTION
     real_t computeEnergy(const real_t& distSqr, const idx_t& typeIdx) const
     {
-        if (distSqr >= cappingDistanceSqr_[typeIdx])
+        if (distSqr >= precomputedValues_(typeIdx).cappingDistanceSqr)
         {
             // normal LJ energy calculation
             real_t frac2 = 1_r / distSqr;
             real_t frac6 = frac2 * frac2 * frac2;
-            return frac6 * (ef1_[typeIdx] * frac6 - ef2_[typeIdx]) - shift_[typeIdx];
+            return frac6 *
+                       (precomputedValues_(typeIdx).ef1 * frac6 - precomputedValues_(typeIdx).ef2) -
+                   precomputedValues_(typeIdx).shift;
         }
 
         // capped energy
-        return energyAtCappingPoint_[typeIdx] -
-               (std::sqrt(distSqr) - cappingDistance_[typeIdx]) * cappingCoeff_[typeIdx] -
-               shift_[typeIdx];
+        return precomputedValues_(typeIdx).energyAtCappingPoint -
+               (std::sqrt(distSqr) - precomputedValues_(typeIdx).cappingDistance) *
+                   precomputedValues_(typeIdx).cappingCoeff -
+               precomputedValues_(typeIdx).shift;
     }
 
     CappedLennardJonesPotential(const std::vector<real_t>& cappingDistance,
@@ -70,55 +77,42 @@ public:
         assert(sigma.size() == numTypes * numTypes);
         assert(epsilon.size() == numTypes * numTypes);
 
-        ff1_.resize(numTypes * numTypes);
-        ff2_.resize(numTypes * numTypes);
-        ef1_.resize(numTypes * numTypes);
-        ef2_.resize(numTypes * numTypes);
-        rcSqr_.resize(numTypes * numTypes);
-        cappingDistance_.resize(numTypes * numTypes);
-        cappingDistanceSqr_.resize(numTypes * numTypes);
-        cappingCoeff_.resize(numTypes * numTypes);
-        shift_.resize(numTypes * numTypes);
-        energyAtCappingPoint_.resize(numTypes * numTypes);
+        precomputedValues_ = Kokkos::View<PrecomputedValues*>(
+            "CappedLennardJonesPotential::PrecomputedValues", numTypes * numTypes);
+        auto hPrecomputedValues =
+            Kokkos::create_mirror_view(Kokkos::HostSpace(), precomputedValues_);
 
         for (idx_t typeIdx = 0; typeIdx < numTypes * numTypes; ++typeIdx)
         {
             auto sig2 = sigma[typeIdx] * sigma[typeIdx];
             auto sig6 = sig2 * sig2 * sig2;
-            ff1_[typeIdx] = 48_r * epsilon[typeIdx] * sig6 * sig6;
-            ff2_[typeIdx] = 24_r * epsilon[typeIdx] * sig6;
-            ef1_[typeIdx] = 4_r * epsilon[typeIdx] * sig6 * sig6;
-            ef2_[typeIdx] = 4_r * epsilon[typeIdx] * sig6;
+            hPrecomputedValues(typeIdx).ff1 = 48_r * epsilon[typeIdx] * sig6 * sig6;
+            hPrecomputedValues(typeIdx).ff2 = 24_r * epsilon[typeIdx] * sig6;
+            hPrecomputedValues(typeIdx).ef1 = 4_r * epsilon[typeIdx] * sig6 * sig6;
+            hPrecomputedValues(typeIdx).ef2 = 4_r * epsilon[typeIdx] * sig6;
 
-            rcSqr_[typeIdx] = rc[typeIdx] * rc[typeIdx];
+            hPrecomputedValues(typeIdx).rcSqr = rc[typeIdx] * rc[typeIdx];
 
             // parameters for the capped part of LJ, use uncapped LJ to compute cappingCoeff
-            cappingDistance_[typeIdx] = 0_r;
-            cappingDistanceSqr_[typeIdx] = 0_r;
-            cappingCoeff_[typeIdx] =
+            hPrecomputedValues(typeIdx).cappingDistance = 0_r;
+            hPrecomputedValues(typeIdx).cappingDistanceSqr = 0_r;
+            hPrecomputedValues(typeIdx).cappingCoeff =
                 computeForce(cappingDistance[typeIdx] * cappingDistance[typeIdx], typeIdx) *
                 cappingDistance[typeIdx];
-            energyAtCappingPoint_[typeIdx] =
+            hPrecomputedValues(typeIdx).energyAtCappingPoint =
                 computeEnergy(cappingDistance[typeIdx] * cappingDistance[typeIdx], typeIdx);
-            cappingDistance_[typeIdx] = cappingDistance[typeIdx];
-            cappingDistanceSqr_[typeIdx] = cappingDistance[typeIdx] * cappingDistance[typeIdx];
+            hPrecomputedValues(typeIdx).cappingDistance = cappingDistance[typeIdx];
+            hPrecomputedValues(typeIdx).cappingDistanceSqr =
+                cappingDistance[typeIdx] * cappingDistance[typeIdx];
 
             if (doShift)
             {
-                shift_[typeIdx] = computeEnergy(rcSqr_[typeIdx], typeIdx);
+                hPrecomputedValues(typeIdx).shift =
+                    computeEnergy(hPrecomputedValues(typeIdx).rcSqr, typeIdx);
             }
         }
 
-        ff1_.shrink_to_fit();
-        ff2_.shrink_to_fit();
-        ef1_.shrink_to_fit();
-        ef2_.shrink_to_fit();
-        rcSqr_.shrink_to_fit();
-        cappingDistance_.shrink_to_fit();
-        cappingDistanceSqr_.shrink_to_fit();
-        cappingCoeff_.shrink_to_fit();
-        shift_.shrink_to_fit();
-        energyAtCappingPoint_.shrink_to_fit();
+        Kokkos::deep_copy(precomputedValues_, hPrecomputedValues);
     }
 };
 }  // namespace impl
