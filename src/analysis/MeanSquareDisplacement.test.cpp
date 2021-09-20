7a6f18e215e29f85f11f0e766f2d26c09c502d3c
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-09-20 17:06:01 +0200

    fixed test case

--- src/analysis/MeanSquareDisplacement.test.cpp
+++ src/analysis/MeanSquareDisplacement.test.cpp
@@ -13,7 +13,8 @@ using MeanSquareDisplacementTest = test::SingleParticle;
 
 TEST_F(MeanSquareDisplacementTest, no_displacement)
 {
-    analysis::MeanSquareDisplacement msd;
+    data::Subdomain subdomain({0_r, 0_r, 0_r}, {10_r, 10_r, 10_r}, 1_r);
+    analysis::MeanSquareDisplacement msd(subdomain);
     msd.reset(atoms);
     auto meanSqDisplacement = msd.calc(atoms);
     EXPECT_FLOAT_EQ(meanSqDisplacement, 0_r);
