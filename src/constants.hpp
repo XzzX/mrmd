17af98964280628f7cc31f1520b1525e01a1f068
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-09-20 11:56:43 +0200

    some common constants

--- src/constants.hpp
+++ src/constants.hpp
@@ -9,4 +9,10 @@ constexpr static int DIMENSIONS = 3;
 
 constexpr real_t pi = 3.14159265358979323846;
 constexpr real_t M_SQRTPI = 1.77245385090551602729;  // sqrt(pi)
+
+namespace constants
+{
+constexpr real_t N_A = 6.02214129e23;  ///< Avogadro constant (1/mol)
+constexpr real_t k_b = 8.3144621e-3;   ///< Boltzmann's constant (kJ/mol/K)
+}  // namespace constants
 }  // namespace mrmd
