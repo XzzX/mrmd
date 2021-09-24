3bcf0005421f61682cb491cf9626d6ccb8b714a2
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-09-24 15:56:39 +0200

    better description

--- src/analysis/MeanSquareDisplacement.hpp
+++ src/analysis/MeanSquareDisplacement.hpp
@@ -16,7 +16,14 @@ private:
     data::Subdomain subdomain_;
 
 public:
+    /**
+     * Store current positions as a starting point.
+     */
     void reset(data::Atoms& atoms);
+    /**
+     * Compare current positions against the starting point
+     * @return mean squre displacement
+     */
     real_t calc(data::Atoms& atoms);
 
     MeanSquareDisplacement(const data::Subdomain& subdomain)
