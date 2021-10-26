d223c1f7bfac5b0740f62a7a6c69b5729723b810
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2021-10-26 12:55:57 +0200

    added multi histogram to track multiple atom specimens

---
+++ src/data/MultiHistogram.cpp
@@ -0,0 +1,59 @@
+#include "MultiHistogram.hpp"
+
+#include <cassert>
+
+namespace mrmd
+{
+namespace data
+{
+MultiHistogram& MultiHistogram::operator+=(const MultiHistogram& rhs)
+{
+    if (numBins != rhs.numBins) exit(EXIT_FAILURE);
+    assert(min == rhs.min);
+    assert(max == rhs.max);
+
+    auto lhs = data;
+    auto policy = Kokkos::MDRangePolicy<>({0, 0}, {numBins, numHistograms});
+    auto kernel = KOKKOS_LAMBDA(const idx_t idx, const idx_t jdx)
+    {
+        lhs(idx, jdx) += rhs.data(idx, jdx);
+    };
+    Kokkos::parallel_for("MultiHistogram::operator+=", policy, kernel);
+    Kokkos::fence();
+    return *this;
+}
+
+data::MultiHistogram gradient(const data::MultiHistogram& input)
+{
+    const auto inverseSpacing = input.inverseBinSize;
+    const auto inverseDoubleSpacing = 0.5_r * input.inverseBinSize;
+
+    data::MultiHistogram grad("gradient", input.min, input.max, input.numBins, input.numHistograms);
+    auto policy = Kokkos::MDRangePolicy<>({0, 0}, {input.numBins, input.numHistograms});
+    auto kernel = KOKKOS_LAMBDA(const idx_t idx, const idx_t jdx)
+    {
+        if (idx == 0)
+        {
+            grad.data(idx, jdx) =
+                (input.data(idx + 1, jdx) - input.data(idx, jdx)) * inverseSpacing;
+            return;
+        }
+
+        if (idx == input.numBins - 1)
+        {
+            grad.data(idx, jdx) =
+                (input.data(idx, jdx) - input.data(idx - 1, jdx)) * inverseSpacing;
+            return;
+        }
+
+        grad.data(idx, jdx) =
+            (input.data(idx + 1, jdx) - input.data(idx - 1, jdx)) * inverseDoubleSpacing;
+    };
+    Kokkos::parallel_for("MultiHistogram::gradient", policy, kernel);
+    Kokkos::fence();
+
+    return grad;
+}
+
+}  // namespace data
+}  // namespace mrmd
---
+++ src/data/MultiHistogram.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <string>
+
+#include "datatypes.hpp"
+
+namespace mrmd
+{
+namespace data
+{
+struct MultiHistogram
+{
+    MultiHistogram(const std::string& label,
+                   const real_t minArg,
+                   const real_t maxArg,
+                   idx_t numBinsArg,
+                   idx_t numHistogramsArg)
+        : min(minArg),
+          max(maxArg),
+          numBins(numBinsArg),
+          numHistograms(numHistogramsArg),
+          binSize((max - min) / real_c(numBins)),
+          inverseBinSize(1_r / binSize),
+          data(label, numBins, numHistograms)
+    {
+    }
+
+    /**
+     * @param val input value
+     * @return corresponding bin or -1 if outside of range
+     */
+    KOKKOS_INLINE_FUNCTION idx_t getBin(const real_t& val) const
+    {
+        auto bin = idx_c((val - min) * inverseBinSize);
+        if (bin < 0) bin = -1;
+        if (bin >= numBins) bin = -1;
+        return bin;
+    }
+
+    const real_t min;
+    const real_t max;
+    const idx_t numBins;
+    const idx_t numHistograms;
+    const real_t binSize;
+    const real_t inverseBinSize;
+    MultiView data;
+
+    MultiHistogram& operator+=(const MultiHistogram& rhs);
+};
+
+data::MultiHistogram gradient(const data::MultiHistogram& input);
+
+}  // namespace data
+}  // namespace mrmd
