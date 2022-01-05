a18df5c5e019e9b31a02fdc147e225f161a31911
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2022-01-05 15:53:19 +0100

    constructor for MPIInfo struct

--- src/data/MPIInfo.hpp
+++ src/data/MPIInfo.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <mpi.h>
+#include "mpi_helper.hpp"
 
 namespace mrmd::data
 {
@@ -9,5 +9,12 @@ struct MPIInfo
     MPI_Comm comm = MPI_COMM_NULL;
     int rank = -1;
     int size = -1;
+
+    explicit MPIInfo(MPI_Comm communicator)
+    {
+        comm = communicator;
+        CHECK_MPI(MPI_Comm_rank(communicator, &rank));
+        CHECK_MPI(MPI_Comm_size(communicator, &size));
+    }
 };
 }  // namespace mrmd::data
