651c11ad971ff5e11593bc495a4e93e0a2d0f898
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2022-01-11 10:49:00 +0100

    use multiple executable for tests
    - add support for parallel tests

---
+++ cmake/TestFunctions.cmake
@@ -0,0 +1,37 @@
+function(mrmd_add_test)
+    if (BUILD_TESTING AND MRMD_ENABLE_TESTING)
+        set(options)
+        set(oneValueArgs NAME PROCESSES)
+        set(multiValueArgs FILES LABELS)
+        cmake_parse_arguments(ARG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+        if (NOT ARG_NAME)
+            message(FATAL_ERROR "No name given for test!")
+        endif ()
+
+        if (NOT ARG_NAME)
+            message(FATAL_ERROR "No files given for test ${ARG_NAME}!")
+        endif ()
+
+        if (NOT ARG_PROCESSES)
+            set(num_processes 1)
+        else ()
+            set(num_processes ${ARG_PROCESSES})
+        endif ()
+
+        add_executable(${ARG_NAME}
+                ${ARG_FILES}
+                )
+        target_link_libraries(${ARG_NAME}
+                PRIVATE mrmd_test_main
+                )
+        add_test(NAME ${ARG_NAME} COMMAND ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${num_processes} ${MPIEXEC_PREFLAGS} ./${ARG_NAME})
+
+        set_tests_properties(${ARG_NAME} PROPERTIES ENVIRONMENT "OMP_PROC_BIND=spread;OMP_PLACES=threads")
+        if (NOT ARG_LABELS)
+            set_tests_properties(${ARG_NAME} PROPERTIES LABELS "${ARG_LABELS}")
+        endif ()
+        set_tests_properties(${ARG_NAME} PROPERTIES PROCESSORS ${num_processes})
+        set_tests_properties(${ARG_NAME} PROPERTIES PROCESSOR_AFFINITY ON)
+    endif ()
+endfunction()
