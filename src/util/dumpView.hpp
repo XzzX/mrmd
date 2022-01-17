14bededb542279eee4520222a3cfbcebcecb2bf4
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2022-01-17 17:50:14 +0100

    fix some warnings

--- src/util/dumpView.hpp
+++ src/util/dumpView.hpp
@@ -12,7 +12,7 @@ void dumpView(const std::string& filename, const ScalarView& view)
 {
     auto data = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), view);
     std::ofstream fout(filename);
-    for (auto i = 0; i < data.extent(0); ++i)
+    for (idx_t i = 0; i < idx_c(data.extent(0)); ++i)
     {
         fout << data(i) << std::endl;
     }
@@ -23,9 +23,9 @@ void dumpView(const std::string& filename, const MultiView& view)
 {
     auto data = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), view);
     std::ofstream fout(filename);
-    for (auto i = 0; i < data.extent(0); ++i)
+    for (idx_t i = 0; i < idx_c(data.extent(0)); ++i)
     {
-        for (auto j = 0; j < data.extent(1); ++j)
+        for (idx_t j = 0; j < idx_c(data.extent(1)); ++j)
         {
             fout << data(i, j) << " ";
         }
