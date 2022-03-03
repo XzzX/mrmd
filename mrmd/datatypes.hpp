192ad7ca55d5fde7c75458f0759e2bb58727cbde
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2022-03-03 09:56:03 +0100

    added kokkos inline to datatypes

--- mrmd/datatypes.hpp
+++ mrmd/datatypes.hpp
@@ -10,69 +10,69 @@ namespace mrmd
 {
 // convenience casting functions for unsigned ints
 template <typename T>
-inline constexpr unsigned int uint_c(const T &val)
+KOKKOS_INLINE_FUNCTION constexpr unsigned int uint_c(const T &val)
 {
     return static_cast<unsigned int>(val);
 }
 
 template <typename T>
-inline constexpr uint8_t uint8_c(const T &val)
+KOKKOS_INLINE_FUNCTION constexpr uint8_t uint8_c(const T &val)
 {
     return static_cast<uint8_t>(val);
 }
 
 template <typename T>
-inline constexpr uint16_t uint16_c(const T &val)
+KOKKOS_INLINE_FUNCTION constexpr uint16_t uint16_c(const T &val)
 {
     return static_cast<uint16_t>(val);
 }
 
 template <typename T>
-inline constexpr uint32_t uint32_c(const T &val)
+KOKKOS_INLINE_FUNCTION constexpr uint32_t uint32_c(const T &val)
 {
     return static_cast<uint32_t>(val);
 }
 
 template <typename T>
-inline constexpr uint64_t uint64_c(const T &val)
+KOKKOS_INLINE_FUNCTION constexpr uint64_t uint64_c(const T &val)
 {
     return static_cast<uint64_t>(val);
 }
 
 // convenience casting functions for signed ints
 template <typename T>
-inline constexpr int int_c(const T &val)
+KOKKOS_INLINE_FUNCTION constexpr int int_c(const T &val)
 {
     return static_cast<int>(val);
 }
 
 template <typename T>
-inline constexpr int8_t int8_c(const T &val)
+KOKKOS_INLINE_FUNCTION constexpr int8_t int8_c(const T &val)
 {
     return static_cast<int8_t>(val);
 }
 
 template <typename T>
-inline constexpr int16_t int16_c(const T &val)
+KOKKOS_INLINE_FUNCTION constexpr int16_t int16_c(const T &val)
 {
     return static_cast<int16_t>(val);
 }
 
 template <typename T>
-inline constexpr int32_t int32_c(const T &val)
+KOKKOS_INLINE_FUNCTION constexpr int32_t int32_c(const T &val)
 {
     return static_cast<int32_t>(val);
 }
 
 template <typename T>
-inline constexpr int64_t int64_c(const T &val)
+KOKKOS_INLINE_FUNCTION constexpr int64_t int64_c(const T &val)
 {
     return static_cast<int64_t>(val);
 }
 
 using idx_t = int64_t;
 template <typename T>
-inline constexpr idx_t idx_c(const T &val)
+KOKKOS_INLINE_FUNCTION constexpr idx_t idx_c(const T &val)
 {
     return static_cast<idx_t>(val);
 }
@@ -80,27 +80,30 @@ inline constexpr idx_t idx_c(const T &val)
 // convenience casting functions for floating point
 using real_t = double;
 
-inline constexpr real_t operator"" _r(const long double val) { return static_cast<real_t>(val); }
+KOKKOS_INLINE_FUNCTION constexpr real_t operator"" _r(const long double val)
+{
+    return static_cast<real_t>(val);
+}
 
-inline constexpr real_t operator"" _r(const unsigned long long val)
+KOKKOS_INLINE_FUNCTION constexpr real_t operator"" _r(const unsigned long long val)
 {
     return static_cast<real_t>(val);
 }
 
 template <typename T>
-inline constexpr real_t real_c(T t)
+KOKKOS_INLINE_FUNCTION constexpr real_t real_c(T t)
 {
     return static_cast<real_t>(t);
 }
 
 template <typename T>
-inline constexpr double double_c(const T &val)
+KOKKOS_INLINE_FUNCTION constexpr double double_c(const T &val)
 {
     return static_cast<double>(val);
 }
 
 template <typename T>
-inline constexpr float float_c(const T &val)
+KOKKOS_INLINE_FUNCTION constexpr float float_c(const T &val)
 {
     return static_cast<float>(val);
 }
