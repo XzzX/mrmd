ddaf8b29b06e96807ca36e6e6a3bff66a4dc07f3
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2022-03-03 15:46:16 +0100

    hdf5 support for python interface

--- pyMRMD/io.cpp
+++ pyMRMD/io.cpp
@@ -2,11 +2,24 @@
 
 #include <io/DumpCSV.hpp>
 #include <io/DumpGRO.hpp>
+#include <io/DumpH5MDParallel.hpp>
+#include <io/RestoreH5MDParallel.hpp>
 
 namespace py = pybind11;
 
 void init_io(py::module_ &m)
 {
-    m.def("dump_csv", &mrmd::io::dumpCSV);
-    m.def("dump_gro", &mrmd::io::dumpGRO);
+    using namespace mrmd;
+    m.def("dump_csv", &io::dumpCSV);
+    m.def("dump_gro", &io::dumpGRO);
+
+    py::class_<io::DumpH5MDParallel>(m, "DumpH5MDParallel")
+        .def(py::init<const std::shared_ptr<data::MPIInfo> &,
+                      const std::string &,
+                      const std::string &>())
+        .def("dump", &io::DumpH5MDParallel::dump);
+
+    py::class_<io::RestoreH5MDParallel>(m, "RestoreH5MDParallel")
+        .def(py::init<const std::shared_ptr<data::MPIInfo> &, const std::string &>())
+        .def("restore", &io::RestoreH5MDParallel::restore);
 }
