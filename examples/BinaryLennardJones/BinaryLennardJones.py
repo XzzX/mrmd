fcbfe865056cc35ac585b6a143617445afcf6df7
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2022-03-17 11:28:05 +0100

    add binary lennard jones python files

---
+++ examples/BinaryLennardJones/BinaryLennardJones.py
@@ -0,0 +1,32 @@
+import numpy as np
+from pprint import pprint
+import pyMRMD
+from initialization import init_simulation, init_molecules
+from NPT import npt
+from SPARTIAN import spartian
+import yaml
+
+config = yaml.load(open('input.yaml', 'r'), Loader=yaml.Loader)
+pprint(config)
+
+pyMRMD.initialize()
+mpi_info = pyMRMD.data.MPIInfo()
+subdomain = pyMRMD.data.Subdomain()
+atoms = pyMRMD.data.DeviceAtoms(0)
+dumper = pyMRMD.io.DumpH5MDParallel(mpi_info, 'XzzX', 'atoms')
+restore = pyMRMD.io.RestoreH5MDParallel(mpi_info, 'atoms')
+#restore.restore('nvt.hdf5', subdomain, atoms)
+#print(subdomain.min_corner, subdomain.max_corner)
+
+subdomain, atoms = init_simulation(config['initialization'])
+dumper.dump('initial.hdf5', subdomain, atoms)
+
+npt(config['NPT'], subdomain, atoms)
+dumper.dump('npt.hdf5', subdomain, atoms)
+
+npt(config['NVT'], subdomain, atoms)
+dumper.dump('nvt.hdf5', subdomain, atoms)
+
+molecules = pyMRMD.data.create_molecule_for_each_atom(atoms)
+spartian(config['SPARTIAN'], subdomain, atoms, molecules)
+dumper.dump('spartian.hdf5', subdomain, atoms)
---
+++ examples/BinaryLennardJones/initialization.py
@@ -0,0 +1,52 @@
+#!python3
+
+import numpy as np
+import pyMRMD
+
+
+def fill_domain_with_atoms_sc(config, subdomain):
+    num_atoms_a = config['num_atoms'] * config['fraction_type_A']
+
+    atoms = pyMRMD.data.HostAtoms(config['num_atoms'])
+
+    pos = atoms.get_pos_np()
+    pos[0, :, :] = np.random.uniform(size=pos.shape[1:]) * subdomain.diameter[0] + subdomain.min_corner[0]
+    pos[1, :, :] = np.random.uniform(size=pos.shape[1:]) * subdomain.diameter[1] + subdomain.min_corner[1]
+    pos[2, :, :] = np.random.uniform(size=pos.shape[1:]) * subdomain.diameter[2] + subdomain.min_corner[2]
+
+    vel = atoms.get_vel_np()
+    vel[:, :, :] = np.random.uniform(-0.5, 0.5, size=vel.shape) * config['max_velocity']
+
+    mass = atoms.get_mass_np()
+    mass[:, :] = 1
+
+    relative_mass = atoms.get_relative_mass_np()
+    relative_mass[:, :] = 1
+
+    type = atoms.get_type_np()
+    type[:, :] = 1
+    type[:, :int(num_atoms_a / type.shape[0])] = 0
+
+    atoms.num_local_atoms = config['num_atoms']
+    atoms.num_ghost_atoms = 0
+
+    return pyMRMD.data.DeviceAtoms(atoms)
+
+def init_molecules(num_atoms):
+    molecules = pyMRMD.data.HostMolecules(2 * num_atoms)
+    
+    offset = molecules.get_atoms_offset_np()
+    offset[:, :] = np.arange(offset.shape[0] * offset.shape[1]).reshape(offset.shape)
+
+    size = molecules.get_num_atoms_np()
+    size[:, :] = 1
+
+    molecules.num_local_molecules = num_atoms;
+
+    return pyMRMD.data.DeviceMolecules(molecules)
+
+def init_simulation(config):
+    subdomain = pyMRMD.data.Subdomain([0, 0, 0], config['box'], config['ghost_layer_thickness'])
+    atoms = fill_domain_with_atoms_sc(config, subdomain)
+    return subdomain, atoms
+
