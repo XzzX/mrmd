0f335c359aeea17fbc98d5d498c49e429ee5f0c0
julianhille <julianhille@zedat.fu-berlin.de>
2025-02-05 14:15:56 +0100

    fixed ApplicationRegion test, Ref #033

--- mrmd/util/ApplicationRegion.test.cpp
+++ mrmd/util/ApplicationRegion.test.cpp
@@ -24,12 +24,18 @@ TEST(ApplicationRegion, testRegion)
 {
     Point3D center = {2_r, 3_r, 4_r};
     const auto applicationRegionMin = 2_r; 
-    const auto applicationRegionMax = 2_r;
+    const auto applicationRegionMax = 4_r;
     auto applicationRegion = ApplicationRegion(center, applicationRegionMin, applicationRegionMax);
 
-    EXPECT_TRUE(applicationRegion.isInApplicationRegion(2_r, 10_r, 10_r));
+    EXPECT_TRUE(applicationRegion.isInApplicationRegion(4_r, 10_r, 10_r));
+    EXPECT_TRUE(applicationRegion.isInApplicationRegion(5.7_r, 10_r, 10_r));
+    EXPECT_TRUE(applicationRegion.isInApplicationRegion(6_r, 10_r, 10_r));
+    EXPECT_TRUE(applicationRegion.isInApplicationRegion(-2_r, 10_r, 10_r));
+    EXPECT_TRUE(applicationRegion.isInApplicationRegion(-1.2_r, 10_r, 10_r));
+    EXPECT_TRUE(applicationRegion.isInApplicationRegion(0_r, 10_r, 10_r));
+
     EXPECT_FALSE(applicationRegion.isInApplicationRegion(3.1_r, 10_r, 10_r));
-    EXPECT_FALSE(applicationRegion.isInApplicationRegion(5.2_r, 10_r, 10_r));
+    EXPECT_FALSE(applicationRegion.isInApplicationRegion(-2.1_r, 10_r, 10_r));
 }
 
 } // namespace util
