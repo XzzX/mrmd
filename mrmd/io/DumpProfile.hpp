e9e15eacaae747df54bd96a0e0a7226f67fb6a7b
julianhille <julianhille@zedat.fu-berlin.de>
2025-03-12 12:11:47 +0100

    added module to dump a data profile to text file, Ref #040

---
+++ mrmd/io/DumpProfile.hpp
@@ -0,0 +1,34 @@
+// Copyright 2024 Sebastian Eibl
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <fstream>
+
+namespace mrmd
+{
+namespace io
+{
+void dumpProfile(std::ofstream& fileProfile, const ScalarView& dataProfile, const real_t& normalizationFactor = 1_r)
+{
+    for (auto i = 0; i < dataProfile.extent(0); ++i)
+    {
+        std::string separator = (i < dataProfile.extent(0) - 1) ? " " : "";
+        fileProfile << dataProfile(i) * normalizationFactor << separator;
+    }
+    fileProfile << std::endl;
+}
+
+}  // namespace io
+}  // namespace mrmd
