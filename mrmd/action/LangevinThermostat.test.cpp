46fff150d5682169d9e0e620bdabaa9201eddcd5
julianhille <julianhille@zedat.fu-berlin.de>
2025-05-20 17:54:38 +0200

    added unit test for local Langevin thermostat, Ref #045

--- mrmd/action/LangevinThermostat.test.cpp
+++ mrmd/action/LangevinThermostat.test.cpp
@@ -18,6 +18,7 @@
 
 #include "data/Atoms.hpp"
 #include "test/SingleAtom.hpp"
+#include "data/Subdomain.hpp"
 
 namespace mrmd
 {
@@ -29,6 +30,43 @@ TEST_F(LangevinThermostatTest, Simple)
 {
     LangevinThermostat langevinThermostat(0.5_r, 0.5_r, 0.1_r);
     langevinThermostat.apply(atoms);
+
+    auto hAoSoA = Cabana::create_mirror_view_and_copy(Kokkos::HostSpace(), atoms.getAoSoA());
+    auto force = Cabana::slice<data::Atoms::FORCE>(hAoSoA);
+    EXPECT_GE(force(0, 0),
+              9_r + langevinThermostat.getPref1() * 1.5_r * 7_r -
+                  langevinThermostat.getPref2() * std::sqrt(1.5_r) * 0.5_r);
+    EXPECT_LE(force(0, 0),
+              9_r + langevinThermostat.getPref1() * 1.5_r * 7_r +
+                  langevinThermostat.getPref2() * std::sqrt(1.5_r) * 0.5_r);
+    EXPECT_GE(force(0, 1),
+              7_r + langevinThermostat.getPref1() * 1.5_r * 5_r -
+                  langevinThermostat.getPref2() * std::sqrt(1.5_r) * 0.5_r);
+    EXPECT_LE(force(0, 1),
+              7_r + langevinThermostat.getPref1() * 1.5_r * 5_r +
+                  langevinThermostat.getPref2() * std::sqrt(1.5_r) * 0.5_r);
+    EXPECT_GE(force(0, 2),
+              8_r + langevinThermostat.getPref1() * 1.5_r * 3_r -
+                  langevinThermostat.getPref2() * std::sqrt(1.5_r) * 0.5_r);
+    EXPECT_LE(force(0, 2),
+              8_r + langevinThermostat.getPref1() * 1.5_r * 3_r +
+                  langevinThermostat.getPref2() * std::sqrt(1.5_r) * 0.5_r);
+}
+
+TEST_F(LangevinThermostatTest, Local)
+{
+    LangevinThermostat langevinThermostat(0.5_r, 0.5_r, 0.1_r);
+    auto subdomain = data::Subdomain({0_r, 0_r, 0_r}, {3_r, 3_r, 3_r}, 0.7_r);
+    
+    real_t boxCenterX = 0.5_r * (subdomain.maxCorner[0] + subdomain.minCorner[0]);
+    real_t boxCenterY = 0.5_r * (subdomain.maxCorner[1] + subdomain.minCorner[1]);
+    real_t boxCenterZ = 0.5_r * (subdomain.maxCorner[2] + subdomain.minCorner[2]);
+
+    auto applicationRegion = util::ApplicationRegion({boxCenterX, boxCenterY, boxCenterZ},
+                                                    0_r,
+                                                    1.5_r);
+
+    langevinThermostat.apply(atoms, applicationRegion);
 
     auto hAoSoA = Cabana::create_mirror_view_and_copy(Kokkos::HostSpace(), atoms.getAoSoA());
     auto force = Cabana::slice<data::Atoms::FORCE>(hAoSoA);
