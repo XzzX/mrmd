3ad7fda87f33ee8087ccc7b1df9972a629adf983
Sebastian Eibl <sebastian.eibl@mpcdf.mpg.de>
2025-11-04 10:32:37 +0100

    move Barostat to c++ file

--- mrmd/action/BerendsenBarostat.hpp
+++ mrmd/action/BerendsenBarostat.hpp
@@ -35,26 +35,7 @@ void apply(data::Atoms& atoms,
            data::Subdomain& subdomain,
            bool stretchX = true,
            bool stretchY = true,
-           bool stretchZ = true)
-{
-    auto mu = std::cbrt(1_r + gamma * (currentPressure - targetPressure));
-    if (stretchX) subdomain.scaleDim(mu, AXIS::X);
-    if (stretchY) subdomain.scaleDim(mu, AXIS::Y);
-    if (stretchZ) subdomain.scaleDim(mu, AXIS::Z);
-
-    auto pos = atoms.getPos();
-
-    auto policy = Kokkos::RangePolicy<>(0, atoms.numLocalAtoms);
-    auto kernel = KOKKOS_LAMBDA(const idx_t& idx)
-    {
-        if (stretchX) pos(idx, to_underlying(AXIS::X)) *= mu;
-        if (stretchY) pos(idx, to_underlying(AXIS::Y)) *= mu;
-        if (stretchZ) pos(idx, to_underlying(AXIS::Z)) *= mu;
-    };
-    Kokkos::parallel_for("BerendsenBarostat::apply", policy, kernel);
-
-    Kokkos::fence();
-}
+           bool stretchZ = true);
 }  // namespace BerendsenBarostat
 }  // namespace action
 }  // namespace mrmd
