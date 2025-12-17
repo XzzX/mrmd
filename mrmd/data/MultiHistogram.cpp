603f4c4b3d031b1851351118d73d31f2f38aceed
julianhille <julianhille@zedat.fu-berlin.de>
2025-12-17 16:01:10 +0100

    avoid capturing this pointer at member function call, Ref. #047

--- mrmd/data/MultiHistogram.cpp
+++ mrmd/data/MultiHistogram.cpp
@@ -28,8 +28,9 @@ ScalarView MultiHistogram::createGrid() const
     MRMD_HOST_CHECK_GREATEREQUAL(numBins, 0);
 
     ScalarView grid("grid", numBins);
+    auto self = *this;  // avoid capturing this pointer
     auto policy = Kokkos::RangePolicy<>(0, numBins);
-    auto kernel = KOKKOS_LAMBDA(const idx_t idx) { grid[idx] = getBinPosition(idx); };
+    auto kernel = KOKKOS_LAMBDA(const idx_t idx) { grid[idx] = self.getBinPosition(idx); };
     Kokkos::parallel_for("MultiHistogram::scale", policy, kernel);
     Kokkos::fence();
 
