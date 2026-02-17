c6355dfbbd0931c2f1b1404c2e3edaf5c3fbcee8
julianhille <julianhille@zedat.fu-berlin.de>
2026-02-17 15:08:20 +0100

    added simulationSetup files from other branch, Ref. #077

---
+++ mrmd/util/simulationSetup.cpp
@@ -0,0 +1,61 @@
+// Copyright 2024 Sebastian Eibl
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/simulationSetup.hpp"
+
+namespace mrmd
+{
+namespace util
+{
+data::Atoms fillDomainWithAtoms(const data::Subdomain& subdomain,
+                                const idx_t& numAtoms,
+                                const real_t& maxVelocity,
+                                const real_t& inputMass)
+{
+    auto RNG = Kokkos::Random_XorShift1024_Pool<>(1234);
+
+    data::Atoms atoms(numAtoms);
+
+    auto pos = atoms.getPos();
+    auto vel = atoms.getVel();
+    auto mass = atoms.getMass();
+    auto type = atoms.getType();
+    auto charge = atoms.getCharge();
+
+    auto policy = Kokkos::RangePolicy<>(0, numAtoms);
+    auto kernel = KOKKOS_LAMBDA(const idx_t idx)
+    {
+        auto randGen = RNG.get_state();
+        pos(idx, 0) = randGen.drand() * subdomain.diameter[0] + subdomain.minCorner[0];
+        pos(idx, 1) = randGen.drand() * subdomain.diameter[1] + subdomain.minCorner[1];
+        pos(idx, 2) = randGen.drand() * subdomain.diameter[2] + subdomain.minCorner[2];
+
+        vel(idx, 0) = (randGen.drand() - 0.5_r) * maxVelocity;
+        vel(idx, 1) = (randGen.drand() - 0.5_r) * maxVelocity;
+        vel(idx, 2) = (randGen.drand() - 0.5_r) * maxVelocity;
+        RNG.free_state(randGen);
+
+        mass(idx) = inputMass;
+        type(idx) = 0;
+        charge(idx) = 0_r;
+    };
+    Kokkos::parallel_for("fillDomainWithAtoms", policy, kernel);
+
+    atoms.numLocalAtoms = numAtoms;
+    atoms.numGhostAtoms = 0;
+    return atoms;
+}
+
+}  // namespace util
+}  // namespace mrmd
